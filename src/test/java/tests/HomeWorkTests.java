d43943349127a6e6a6bf1425cb9a738dd34d80fe
Krest_IV <krestsov.iv@yandex.ru>
2024-05-16 13:02:32 +0200

    Refactored date-picker removed comments added fix for "element click intercepted"

--- src/test/java/tests/HomeWorkTests.java
+++ src/test/java/tests/HomeWorkTests.java
@@ -3,12 +3,10 @@ package tests;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.Keys;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.by;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
 
 public class HomeWorkTests {
     @BeforeAll
@@ -20,36 +18,35 @@ public class HomeWorkTests {
     @Test
     void fillFormSubmitTest(){
         open("/automation-practice-form");
+        executeJavaScript("$('#fixedban').remove()");
+        executeJavaScript("$('footer').remove()");
 
-        //Simple stuff
         $("#firstName").setValue("Pupa");
         $("#lastName").setValue("Lupa");
         $("#userEmail").setValue("lupa@mail.hr");
         $(by("for","gender-radio-1")).click();
         $("#userNumber").setValue("9005001234");
 
-        //Calendar struggles
         $("#dateOfBirthInput").click();
-        $("#dateOfBirthInput").press(Keys.chord(Keys.CONTROL, "a"));
-        $("#dateOfBirthInput").press("1","7", Keys.SPACE, "M","a", "r", Keys.SPACE, "1","9","8","8");
-        $("#dateOfBirthInput").pressEnter();
+        $(".react-datepicker__year-select").selectOptionByValue("1991");
+        $(".react-datepicker__month-select").selectOptionByValue("4");
+        $(".react-datepicker__day--003").click();
 
-        //Autocomplete input
         $("#subjectsContainer").click();
         $("#subjectsInput").press("m");
         $("#subjectsInput").pressEnter();
 
-        //Only imaginary test-person might have enough time for all these activities
+
         $(by("for","hobbies-checkbox-1")).click();
         $(by("for","hobbies-checkbox-2")).click();
         $(by("for","hobbies-checkbox-3")).click();
 
-        //Загрузка файла из ресурсов проекта
+
         $("#uploadPicture").uploadFromClasspath("ProfilePicture.jpeg");
 
         $("#currentAddress").setValue("Middle-earth, near big lidless Eye");
 
-        //Выбор региона
+
         $("#stateCity-wrapper .css-1wa3eu0-placeholder").click();
         $("#react-select-3-input").pressEnter();
         $("#stateCity-wrapper .css-1wa3eu0-placeholder:nth-child(1)").click();
@@ -57,12 +54,12 @@ public class HomeWorkTests {
 
         $("#submit").click();
 
-        //check the result
+
         $(".modal-content").shouldHave(text("Pupa Lupa"));
         $(".modal-content").shouldHave(text("lupa@mail.hr"));
         $(".modal-content").shouldHave(text("Male"));
         $(".modal-content").shouldHave(text("9005001234"));
-        $(".modal-content").shouldHave(text("17 March,1988"));
+        $(".modal-content").shouldHave(text("3 May,1991"));
         $(".modal-content").shouldHave(text("Maths"));
         $(".modal-content").shouldHave(text("Sports, Reading, Music"));
         $(".modal-content").shouldHave(text("ProfilePicture.jpeg"));
